3a985ef71987ab63701701839ae26ce6781a2120
yangsheng <wu0306109@gmail.com>
2022-03-21 11:31:51 +0800

    Add projection functions

--- gspmi.py
+++ gspmi.py
@@ -2,7 +2,7 @@ from typing import Callable, Generator, Hashable, List, NamedTuple, Set
 
 
 class Item(NamedTuple):
-    """ Item of sequence.
+    """Item of sequence.
     
     interval: interval from zero without itemize transformation
     """
@@ -12,7 +12,7 @@ class Item(NamedTuple):
 
 
 class Pair(NamedTuple):
-    """ Pair of sequence.
+    """Pair of sequence.
 
     interval: interval from previous pair after itemize transformation
     """
@@ -25,7 +25,7 @@ def generate_postfixes(sequence: List[Item],
                        projector: Pair,
                        itemize: Callable[[int], int],
                        level1: bool = False) -> Generator[Item, None, None]:
-    """ Yield postfixs according to projector.
+    """Yield postfixs according to projector.
 
     The function will yield all element-matched postfix from begining for 
     level1 == True; otherwise only one exactly interval-element matched postfix.
@@ -47,3 +47,38 @@ def generate_postfixes(sequence: List[Item],
 
             if not level1:
                 break
+
+
+def project_level1(sequences: List[List[Item]], projector: Pair,
+                   itemize: Callable[[int], int]) -> List[List[List[Item]]]:
+    """Project from initial sequence database.
+
+    Each sequence in database will be project to a list of postfix.
+    """
+    projected_db = []
+    for sequence in sequences:
+        if (postfixes := list(
+                generate_postfixes(sequence, projector, itemize,
+                                   level1=True))):
+            projected_db.append(postfixes)
+
+    return projected_db
+
+
+def project(projected_db: List[List[List[Item]]], projector: Pair,
+            itemize: Callable[[int], int]) -> List[List[List[Item]]]:
+    """Project projected database to next level."""
+    child_projected_db = []
+    for sequences in projected_db:
+        projected_sequences = []
+
+        for sequence in sequences:
+            postfix = next(generate_postfixes(sequence, projector, itemize),
+                           None)
+            if postfix:
+                projected_sequences.append(postfix)
+
+        if projected_sequences:
+            child_projected_db.append(projected_sequences)
+
+    return child_projected_db
--- tests/test_gspmi.py
+++ tests/test_gspmi.py
@@ -1,4 +1,5 @@
-from sequential_pattern_mining.gspmi import generate_postfixes, Item, Pair
+from sequential_pattern.gspmi import (Item, Pair, generate_postfixes,
+                                      project_level1, project)
 
 
 def test_generate_postfixes():
@@ -35,4 +36,101 @@ def test_generate_postfixes():
         Item(172800, {'a', 'c'}),
     ]
     result = next(generate_postfixes(sequence, pair, itemize), None)
+    assert result == expected
+
+
+def test_project_level1():
+    sequences = [
+        [
+            Item(0, {'a'}),
+            Item(86400, {'a', 'b', 'c'}),
+            Item(259200, {'a', 'c'}),
+        ],
+        [
+            Item(0, {'a', 'd'}),
+            Item(259200, {'c'}),
+        ],
+        [
+            Item(0, {'a', 'e', 'f'}),
+            Item(172800, {'a', 'b'}),
+        ],
+    ]
+    projector = Pair(0, 'a')
+    itemize = lambda interval: interval // 86400
+    expected = [
+        [
+            [
+                Item(86400, {'a', 'b', 'c'}),
+                Item(259200, {'a', 'c'}),
+            ],
+            [
+                Item(0, {'b', 'c'}),
+                Item(172800, {'a', 'c'}),
+            ],
+            [
+                Item(0, {'c'}),
+            ],
+        ],
+        [
+            [
+                Item(0, {'d'}),
+                Item(259200, {'c'}),
+            ],
+        ],
+        [
+            [
+                Item(0, {'e', 'f'}),
+                Item(172800, {'a', 'b'}),
+            ],
+            [
+                Item(0, {'b'}),
+            ],
+        ],
+    ]
+    result = project_level1(sequences, projector, itemize)
+    assert result == expected
+
+
+def test_project():
+    projeced_db = [
+        [
+            [
+                Item(86400, {'a', 'b', 'c'}),
+                Item(259200, {'a', 'c'}),
+            ],
+            [
+                Item(0, {'b', 'c'}),
+                Item(172800, {'a', 'c'}),
+            ],
+            [
+                Item(0, {'c'}),
+            ],
+        ],
+        [
+            [
+                Item(0, {'d'}),
+                Item(259200, {'c'}),
+            ],
+        ],
+        [
+            [
+                Item(0, {'e', 'f'}),
+                Item(172800, {'a', 'b'}),
+            ],
+            [
+                Item(0, {'b'}),
+            ],
+        ],
+    ]
+    projector = Pair(0, 'b')
+    itemize = lambda interval: interval // 86400
+    expected = [
+        [
+            [
+                Item(0, {'c'}),
+                Item(172800, {'a', 'c'}),
+            ],
+        ],
+    ]
+    result = project(projeced_db, projector, itemize)
     assert result == expected
