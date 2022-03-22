8b81b5dbe33fe8170eec7a3a34e0352313dbb7e5
yangsheng <wu0306109@gmail.com>
2022-03-22 14:07:33 +0800

    Add a complex test case for mine()

--- tests/test_gspmi.py
+++ tests/test_gspmi.py
@@ -1,4 +1,4 @@
-from math import inf
+from math import inf, log2
 
 from ..gspmi import (Item, Pair, Pattern, generate_postfixes, mine,
                      mine_subpatterns, project, project_level1)
@@ -216,3 +216,82 @@ def test_mine():
     ]
     result = mine(sequence_db, itemize, min_support, max_interval=max_interval)
     assert sorted(result) == sorted(expected)
+
+
+def test_complex_sequences():
+    # Only make sure every found patterns is actually exists.
+    a = 'a'
+    b = 'b'
+    c = 'c'
+    sequences = [
+        [
+            Item(0, {a}),
+            Item(1, {a, b, c}),
+            Item(4, {a}),
+            Item(12, {b, c}),
+            Item(44, {a, b}),
+            Item(45, {a, b}),
+            Item(67, {b, c}),
+        ],
+        [
+            Item(0, {a}),
+            Item(1, {a}),
+            Item(7, {a, b}),
+            Item(14, {a, b, c}),
+            Item(77, {a}),
+            Item(78, {b}),
+        ],
+        [
+            Item(0, {a}),
+            Item(6, {a, b, c}),
+            Item(10, {b, c}),
+            Item(66, {a, c}),
+            Item(67, {b, c}),
+        ],
+        [
+            Item(0, {a}),
+            Item(1, {a, b, c}),
+            Item(79, {a, b}),
+        ],
+        [
+            Item(0, {a}),
+            Item(1, {a, b, c}),
+            Item(127, {a, b}),
+        ],
+    ]
+    itemize = lambda t: int(log2(t + 1))
+    patterns = mine(sequences, itemize, min_support=2)
+
+    def project(projector, postfix):
+        # Return projected postfix if exists, otherwise return none
+        for i, (interval, elements) in enumerate(postfix):
+            if (projector.interval == itemize(interval)
+                    and projector.element in elements):
+                return [Item(ii - interval, ee) for ii, ee in postfix[i:]]
+        else:
+            return None
+
+    def match_postfix(pattern, postfix):
+        for pair in pattern.sequence:
+            if (postfix := project(pair, postfix)) is None:
+                return False
+        else:
+            return True
+
+    def match_sequence(pattern, sequence):
+        first = pattern.sequence[0]
+
+        for i, (interval, elements) in enumerate(sequence):
+            if first.element in elements:
+                postfix = [Item(i - interval, e) for i, e in sequence[i:]]
+
+                if match_postfix(pattern, postfix):
+                    return True
+
+        else:
+            return False
+
+    for pattern in patterns:
+        count = sum(match_sequence(pattern, s) for s in sequences)
+
+        assert count == pattern.support
