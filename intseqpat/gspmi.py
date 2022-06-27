01fe796d435fbc020ef506a1118248aa4daac59b
yangsheng <wu0306109@gmail.com>
2022-06-27 11:06:14 +0800

    Updating algorithm implementation with Pandas.

--- intseqpat/gspmi.py
+++ intseqpat/gspmi.py
@@ -1,257 +1,13 @@
-from collections import Counter
-from math import inf, isinf
-from multiprocessing import Pool
-from typing import (Callable, Generator, Hashable, List, NamedTuple, Set,
-                    Tuple, Union)
-from grpc import Call
-import pandas as pd
+from __future__ import annotations
 
-_Itemize = Callable[[int], int]
+from collections import Counter
+from math import inf
+from typing import Callable, List, NamedTuple, Set, Tuple, Union
+
+import pandas as pd
 
 
 class Item(NamedTuple):
-    """Item of sequence.
-
-    interval: interval from zero without itemize transformation
-    """
-
-    interval: int
-    elements: Set[Hashable]
-
-
-class Pair(NamedTuple):
-    """Pair of sequence.
-
-    interval: interval from previous pair after itemize transformation
-    """
-
-    interval: int
-    element: Hashable
-
-
-class Pattern(NamedTuple):
-    """Mined pattern
-
-    Attributes:
-        support: count of pattern occcurence
-        whole_interval: interval from begining to the end 
-            based on itemized intervals
-    """
-
-    sequence: List[Pair]
-    support: int
-    whole_interval: int
-
-
-_BuildInSequence = List[Tuple[int, Set[Hashable]]]
-_Sequence = List[Item]
-
-
-def transform(sequences: List[_BuildInSequence]) -> List[List[Item]]:
-    """Transform sequences with Python build-in types
-    to fit the format using by GSPMI."""
-    return [[Item(interval, elements) for interval, elements in sequence]
-            for sequence in sequences]
-
-
-class Gspmi:
-    """Algorithm object for 
-    Generailized Sequential Pattern Mining with Interval.
-
-    Attributes:
-        itemize: itemize function
-        min_support: minimal count of pattern occurence.
-        min_interval: minimum interval between each adjacent items
-        max_interval: maximum interval between each adjacent items
-        min_whole_interval: minimum interval from begining to the end, 
-            base on itemized interval (to be updated)
-        max_whole_interval: maximum interval from begining to the end, 
-            base on itemized interval (to be updated).
-        multiprocessing: whether to run with multiprocessing, if ture, 
-            inputed itemize must be picklizeable, please refer: 
-            https://docs.python.org/3/library/pickle.html#what-can-be-pickled-and-unpickled
-        n_processes: number of worker processes to use 
-            (valid only when multiprocessing is true). If processes 
-            is None then the number returned by os.cpu_count() is used.
-    """
-
-    def __init__(self,
-                 itemize: _Itemize,
-                 min_support: int,
-                 min_interval: int = 0,
-                 max_interval: int = inf,
-                 min_whole_interval: int = 0,
-                 max_whole_interval: int = inf,
-                 multiprocessing: bool = False,
-                 n_processes: int = None):
-        self.itemize = itemize
-        self.min_support = min_support
-        self.min_interval = min_interval
-        self.max_interval = max_interval
-        self.min_whole_interval = min_whole_interval
-        self.max_whole_interval = max_whole_interval
-
-        self.multiprocessing = multiprocessing
-        self.n_processes = n_processes
-
-    def _generate_postfixes(
-            self,
-            sequence: _Sequence,
-            projector: Pair,
-            level1: bool = False) -> Generator[Item, None, None]:
-        """Yield postfixs according to projector.
-
-        The function will yield all element-matched postfix from begining for 
-        level1 == True; otherwise only one exactly interval-and-element 
-        matched postfix. If no matched, yield nothing.
-        """
-
-        def exclude(elements):
-            elements = sorted(elements)
-            return set(elements[elements.index(projector.element) + 1:])
-
-        for i, (interval, elements) in enumerate(sequence):
-            if (projector.element in elements and
-                (level1 or self.itemize(interval) == projector.interval)):
-                head = exclude(elements)
-                postfix = [Item(0, head)] if head else []
-
-                postfix.extend(
-                    Item(i - interval, e) for i, e in sequence[i + 1:])
-                yield postfix
-
-                if not level1:
-                    break
-
-    def _project_level1(self, sequences: List[_Sequence],
-                        projector: Pair) -> List[List[List[Item]]]:
-        """Project from initial sequence database.
-
-        Each sequence in database will be project to a list of postfix 
-        named projected database.
-        """
-        projected_db = []
-        for sequence in sequences:
-            if (postfixes := list(
-                    self._generate_postfixes(sequence, projector,
-                                             level1=True))):
-                projected_db.append(postfixes)
-
-        return projected_db
-
-    def _project(self, projected_db: List[List[_Sequence]],
-                 projector: Pair) -> List[List[_Sequence]]:
-        """Project projected database to the next level by projector."""
-        child_projected_db = []
-        for sequences in projected_db:
-            projected_sequences = []
-
-            for sequence in sequences:
-                postfix = next(self._generate_postfixes(sequence, projector),
-                               None)
-                if postfix:
-                    projected_sequences.append(postfix)
-
-            if projected_sequences:
-                child_projected_db.append(projected_sequences)
-
-        return child_projected_db
-
-    def _mine_subpatterns(self, projected_db: List[List[_Sequence]],
-                          prefix: List[Pair]) -> List[Pattern]:
-        """Recursivly mine sub patterns.
-
-        Args:
-            prefix: list of projectors generate during projections
-        """
-        counter = Counter()
-        for sequences in projected_db:
-            pairs = set()
-
-            for sequence in sequences:
-                previous = 0
-                for interval, elements in sequence:
-                    if self.min_interval <= interval - previous <= self.max_interval:
-                        pairs.update(
-                            Pair(self.itemize(interval), e) for e in elements)
-
-                    previous = interval
-
-            counter.update(pairs)
-
-        patterns = []
-        for pair, support in counter.items():
-            whole_interval = sum(i for i, p in prefix) + pair.interval
-            if (support >= self.min_support and
-                (isinf(self.max_whole_interval)
-                 or whole_interval <= self.itemize(self.max_whole_interval))):
-                child_projected_db = self._project(projected_db, pair)
-
-                patterns.extend(
-                    self._mine_subpatterns(child_projected_db,
-                                           prefix + [pair]))
-
-                if whole_interval >= self.itemize(self.min_whole_interval):
-                    patterns.append(
-                        Pattern(prefix + [pair], support, whole_interval))
-
-        return patterns
-
-    def _mine_subpatterns_level1(self, sequences: List[_Sequence],
-                                 element: Hashable,
-                                 support: int) -> List[Pattern]:
-        patterns = []
-
-        if support >= self.min_support:
-            pair = Pair(0, element)
-            if pair.interval >= self.min_whole_interval:
-                patterns.append(Pattern([pair], support, 0))
-
-            projected_db = self._project_level1(sequences, pair)
-            patterns.extend(self._mine_subpatterns(projected_db, [pair]))
-
-        return patterns
-
-    def mine_patterns(
-        self, sequences: Union[List[_Sequence], List[_BuildInSequence]]
-    ) -> List[Pattern]:
-        """Run the algorithm and mine patterns.
-
-        Transform sequences if passing with build-in types.
-        """
-        if len(sequences) > 0 and not isinstance(sequences[0][0], Item):
-            sequences = transform(sequences)
-
-        counter = Counter()
-        for sequence in sequences:
-            elements = set()
-            for item in sequence:
-                elements.update(item.elements)
-
-            counter.update(elements)
-
-        patterns = []
-
-        if not self.multiprocessing:
-            for element, support in counter.items():
-                subpatterns = self._mine_subpatterns_level1(
-                    sequences, element, support)
-                patterns.extend(subpatterns)
-        else:
-            results = []
-            with Pool(self.n_processes) as pool:
-                for element, support in counter.items():
-                    result = pool.apply_async(self._mine_subpatterns_level1,
-                                              (sequences, element, support))
-                    results.append(result)
-
-                for result in results:
-                    patterns.extend(result.get())
-
-        return patterns
-
-
-class Item:
     """Item of input interval seqences.
 
     Attributes:
@@ -259,41 +15,120 @@ class Item:
         elements: Elements of the item.
     """
 
-    def __init__(self, interval: int, elements: Set[str]) -> None:
-        pass
+    interval: int
+    elements: Set[str]
+
+    def exclude(self, element: str) -> Item:
+        """Exclude previous elements according to the alphabet order."""
+        elements = sorted(self.elements)
+        elements = set(elements[elements.index(element) + 1:])
+        return Item(self.interval, elements)
+
+    def minus(self, interval: int) -> Item:
+        """Minus interal of this item."""
+        return Item(self.interval - interval, self.elements)
 
 
-class Pair:
+class Pair(NamedTuple):
     """Pair including interval and element of output pattern.
 
     Attributes:
         interval: Interval from previous pair (after itemize transformation).
-        elements: Element of the pair.
+        element: Element of the pair.
     """
 
-    def __init__(self, interval: int, element: str) -> None:
-        pass
+    interval: int
+    element: str
 
 
-def transform(sequences: List[Tuple[int, Set[Hashable]]]) -> List[List[Item]]:
-    """Transform sequences to defined classes for following calculations of GSPMI.
+class Pattern(NamedTuple):
+    """Interval sequential pattern.
 
     Args:
-        sequences: Input sequences for GSPMI.
+        sequence: The interval sequence.
+        support: Number of sequences the pattern exists.
+        count: Number of places the pattern exists.
+        whole_interval: the interval from the first to the last pair.
+    """
 
-    Returns:
-        Transformed sequences with defined class Item.
+    sequence: List[Pair]
+    support: int
+    count: int
+    whole_interval: int
+
+
+def generate_postfixes(sequence: List[Item],
+                       projector: str) -> List[List[Item]]:
+    """Find projector in each place of sequence and generate postfix.
+
+    Args:
+        projector (str): The element to match.
+    """
+    postfixes = []
+    for i, item in enumerate(sequence):
+        if projector in item.elements:
+            postfix = [it.minus(item.interval) for it in sequence[i + 1:]]
+
+            first = item.exclude(projector)
+            if first.elements:
+                postfix.insert(0, first.minus(item.interval))
+
+            if postfix:
+                postfixes.append(postfix)
+
+    return postfixes
+
+
+def postfix(sequence: List[Item], projector: Pair,
+            itemize: Callable[[int], int]) -> List[Item]:
+    """Generate postfix according to sequence and projector.
+
+    Args:
+        projector (Pair): The pair to match.
+    """
+    for i, item in enumerate(sequence):
+        if (projector.element in item.elements
+                and itemize(item.interval) == projector.interval):
+            postfix = [it.minus(item.interval) for it in sequence[i + 1:]]
+
+            first = item.exclude(projector.element)
+            if first.elements:
+                postfix.insert(0, first.minus(item.interval))
+
+            return postfix
+    else:
+        return []
+
+
+def project(
+    sequences: pd.Series,
+    itemize: Callable[[int], int],
+    prefix: List[Pair],
+    min_support: int,
+    min_interval: int,
+    max_interval: int,
+    min_whole_interval: int,
+    max_whole_interval: int,
+) -> List[Pattern]:
+    """Apply algorithm on sequences after postfixes generating.
+    
+    Args:
+        sequences: Pandas Series with two level index: first is the orgin SN, 
+            second level represent generated postfixes in each place of the seqeuce.
+        prefix: List of Previous projectors.
     """
     pass
 
 
-def gspmi(sequences: Union[List[List[Item]], List[Tuple[int, Set[Hashable]]]],
-          itemize: Callable[[int], int],
-          min_support: Union[float, int],
-          min_interval: int = 0,
-          max_interval: int = inf,
-          min_whole_interval: int = 0,
-          max_whole_interval: int = inf) -> pd.DataFrame:
+def gspmi(
+    sequences: Union[List[List[Item]], List[Tuple[int, Set[str]]]],
+    itemize: Callable[[int], int],
+    min_support: Union[float, int],
+    min_interval: int = 0,
+    max_interval: int = inf,
+    min_whole_interval: int = 0,
+    max_whole_interval: int = inf,
+) -> List[Pattern]:
     """Apply algorithm on sequences to find interval sequential patterns.
 
     For four types of constrains (min/max interval and min/max whole interval),
@@ -317,31 +152,4 @@ def gspmi(sequences: Union[List[List[Item]], List[Tuple[int, Set[Hashable]]]],
         3. count: Number of places the pattern exists.
         3. whole_interval: the interval from the first to the last pair.
     """
-    pass
-
-
-def generate_postfixes(sequence: List[Item],
-                       projector: str) -> List[List[Item]]:
-    """Find projector in each place of sequence and generate postfix.
-
-    Args:
-        projector (str): The element to match.
-    """
-    pass
-
-
-def postfix(sequence: List[Item], projector: Pair,
-            itemize: Callable[[int], int]) -> List[Item]:
-    """Generate postfix according to sequence and projector.
-
-    Args:
-        projector (Pair): The pair to match.
-    """
-    pass
-
-
-def project(sequences: pd.Series, itemize: Callable[[int], int],
-            min_support: int, min_interval: int, max_interval: int,
-            min_whole_interval: int, max_whole_interval: int) -> pd.DataFrame:
-    """Apply algorithm on sequences after postfixes generating."""
     pass
--- tests/test_gspmi.py
+++ tests/test_gspmi.py
@@ -1,191 +1,109 @@
-from math import log2
+from typing import Callable, List
 
-from intseqpat.gspmi import Gspmi, Item, Pair, Pattern, transform
+import pandas as pd
+import pytest
+from intseqpat.gspmi import Item, Pair, Pattern, generate_postfixes, gspmi, postfix, project
 
 
-def test_transform():
-    a, b, c = 'a', 'b', 'c'
-
-    sequences = [
-        [(0, {a}), (86400, {a, b, c}), (259200, {a, c})],
-        [(0, {a, c}), (259200, {c})],
-        [(0, {a, b, c}), (172800, {a, b})],
-    ]
-    expected = [
+@pytest.fixture
+def sequences() -> List[List[Item]]:
+    return [
         [
-            Item(0, {a}),
-            Item(86400, {a, b, c}),
-            Item(259200, {a, c}),
+            Item(0, {'a'}),
+            Item(86400, {'a', 'b', 'c'}),
+            Item(259200, {'a', 'c'}),
         ],
         [
-            Item(0, {a, c}),
-            Item(259200, {c}),
+            Item(0, {'a', 'd'}),
+            Item(259200, {'c'}),
         ],
         [
-            Item(0, {a, b, c}),
-            Item(172800, {a, b}),
+            Item(0, {'a', 'e', 'f'}),
+            Item(172800, {'a', 'b'}),
         ],
     ]
 
-    result = transform(sequences)
-    assert result == expected
+
+class TestItem:
+
+    def test_exclude(self) -> None:
+        item = Item(0, {'g', 'c', 'd', 'a', 'b', 'e', 'f'})
+        assert item.exclude('g').elements == set()
+        assert item.exclude('f').elements == {'g'}
+        assert item.exclude('b').elements == {'g', 'c', 'd', 'e', 'f'}
+
+    def test_minus(self) -> None:
+        item = Item(100, {})
+        assert item.minus(0).interval == 100
+        assert item.minus(50).interval == 50
+        assert item.minus(100).interval == 0
 
 
-def _itemize(t):
-    return t // 86400
+def test_generate_postfixes(sequences: List[List[Item]]) -> None:
+    assert generate_postfixes(sequences[0], 'a') == [
+        [
+            Item(86400, {'a', 'b', 'c'}),
+            Item(259200, {'a', 'c'}),
+        ],
+        [
+            Item(0, {'b', 'c'}),
+            Item(172800, {'a', 'c'}),
+        ],
+        [
+            Item(0, {'c'}),
+        ],
+    ]
 
 
-class TestGspmi:
+def test_postfix() -> None:
+    assert postfix(
+        sequence=[
+            Item(0, {'b', 'c'}),
+            Item(172800, {'a', 'c'}),
+        ],
+        projector=Pair(0, 'b'),
+        itemize=lambda t: t // 86400,
+    ) == [
+        Item(0, {'c'}),
+        Item(172800, {'a', 'c'}),
+    ]
 
-    def test_basic_mine_patterns(self):
-        sequences = [
-            [
-                Item(0, {'a'}),
-                Item(86400, {'a', 'b', 'c'}),
-                Item(259200, {'a', 'c'}),
-            ],
-            [
-                Item(0, {'a', 'd'}),
-                Item(259200, {'c'}),
-            ],
-            [
-                Item(0, {'a', 'e', 'f'}),
-                Item(172800, {'a', 'b'}),
-            ],
-        ]
-        gspmi = Gspmi(itemize=lambda i: i // 86400,
-                      min_support=2,
-                      max_interval=172800)
-        expected = [
-            Pattern([Pair(0, 'a')], 3, 0),
-            Pattern([Pair(0, 'b')], 2, 0),
-            Pattern([Pair(0, 'c')], 2, 0),
-            Pattern([Pair(0, 'a'), Pair(0, 'b')], 2, 0),
-            Pattern([Pair(0, 'a'), Pair(2, 'a')], 2, 2),
-        ]
 
-        result = gspmi.mine_patterns(sequences)
-        assert sorted(result) == sorted(expected)
-
-    def test_complex_mine_patterns(self):
-        # Only make sure every found patterns is actually exists.
-        a = 'a'
-        b = 'b'
-        c = 'c'
-        sequences = [
-            [
-                Item(0, {a}),
-                Item(1, {a, b, c}),
-                Item(4, {a}),
-                Item(12, {b, c}),
-                Item(44, {a, b}),
-                Item(45, {a, b}),
-                Item(67, {b, c}),
-            ],
-            [
-                Item(0, {a}),
-                Item(1, {a}),
-                Item(7, {a, b}),
-                Item(14, {a, b, c}),
-                Item(77, {a}),
-                Item(78, {b}),
-            ],
-            [
-                Item(0, {a}),
-                Item(6, {a, b, c}),
-                Item(10, {b, c}),
-                Item(66, {a, c}),
-                Item(67, {b, c}),
-            ],
-            [
-                Item(0, {a}),
-                Item(1, {a, b, c}),
-                Item(79, {a, b}),
-            ],
-            [
-                Item(0, {a}),
-                Item(1, {a, b, c}),
-                Item(127, {a, b}),
-            ],
-        ]
-        itemize = lambda t: int(log2(t + 1))
-        gspmi = Gspmi(itemize=itemize, min_support=2)
-        patterns = gspmi.mine_patterns(sequences)
-
-        def project(projector, postfix):
-            # Return projected postfix if exists, otherwise return none
-            for i, (interval, elements) in enumerate(postfix):
-                if (projector.interval == itemize(interval)
-                        and projector.element in elements):
-                    return [Item(ii - interval, ee) for ii, ee in postfix[i:]]
-            else:
-                return None
-
-        def match_postfix(pattern, postfix):
-            for pair in pattern.sequence:
-                if (postfix := project(pair, postfix)) is None:
-                    return False
-            else:
-                return True
-
-        def match_sequence(pattern, sequence):
-            first = pattern.sequence[0]
-
-            for i, (interval, elements) in enumerate(sequence):
-                if first.element in elements:
-                    postfix = [Item(i - interval, e) for i, e in sequence[i:]]
-
-                    if match_postfix(pattern, postfix):
-                        return True
-
-            else:
-                return False
-
-        for pattern in patterns:
-            count = sum(match_sequence(pattern, s) for s in sequences)
-
-            assert count == pattern.support
-
-    def test_auto_transform(self):
-        a, b, c, d, e, f = 'a', 'b', 'c', 'd', 'e', 'f'
-        sequences = [
-            [(0, {a}), (86400, {a, b, c}), (259200, {a, c})],
-            [(0, {a, d}), (259200, {c})],
-            [(0, {a, e, f}), (172800, {a, b})],
-        ]
-        gspmi = Gspmi(itemize=lambda i: i // 86400,
-                      min_support=2,
-                      max_interval=172800)
-        expected = [
-            Pattern([Pair(0, 'a')], 3, 0),
-            Pattern([Pair(0, 'b')], 2, 0),
-            Pattern([Pair(0, 'c')], 2, 0),
-            Pattern([Pair(0, 'a'), Pair(0, 'b')], 2, 0),
-            Pattern([Pair(0, 'a'), Pair(2, 'a')], 2, 2),
-        ]
-
-        result = gspmi.mine_patterns(sequences)
-        assert sorted(result) == sorted(expected)
-
-    def test_multiprocessing_mine_patterns(self):
-
-        a, b, c, d, e, f = 'a', 'b', 'c', 'd', 'e', 'f'
-        sequences = [
-            [(0, {a}), (86400, {a, b, c}), (259200, {a, c})],
-            [(0, {a, d}), (259200, {c})],
-            [(0, {a, e, f}), (172800, {a, b})],
-        ]
-        gspmi = Gspmi(itemize=_itemize,
-                      min_support=2,
-                      max_interval=172800,
-                      multiprocessing=True)
-        expected = [
-            Pattern([Pair(0, 'a')], 3, 0),
-            Pattern([Pair(0, 'b')], 2, 0),
-            Pattern([Pair(0, 'c')], 2, 0),
-            Pattern([Pair(0, 'a'), Pair(0, 'b')], 2, 0),
-            Pattern([Pair(0, 'a'), Pair(2, 'a')], 2, 2),
-        ]
-
-        result = gspmi.mine_patterns(sequences)
-        assert sorted(result) == sorted(expected)
+def test_project():
+    sequences = pd.Series({
+        (0, 0): [
+            Item(86400, {'a', 'b', 'c'}),
+            Item(259200, {'a', 'c'}),
+        ],
+        (0, 1): [
+            Item(0, {'b', 'c'}),
+            Item(172800, {'a', 'c'}),
+        ],
+        (0, 2): [
+            Item(0, {'c'}),
+        ],
+        (1, 0): [
+            Item(0, {'d'}),
+            Item(259200, {'c'}),
+        ],
+        (2, 0): [
+            Item(0, {'e', 'f'}),
+            Item(172800, {'a', 'b'}),
+        ],
+        (2, 1): [
+            Item(0, {'b'}),
+        ],
+    })
+    project(
+        sequences=sequences,
+        itemize=lambda t: t // 86400,
+        prefix=[Pair(0, {'a'})],
+        min_support=2,
+    ) == [
+        Pattern(
+            sequence=[Pair(0, 'a'), Pair(0, 'b')],
+            support=2,
+            count=2,
+            whole_interval=0,
+        ),
+    ]
